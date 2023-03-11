30485eab7466f5e7597c85e45870e71e013cacbd
sasha <sashaAriapor@gmail.com>
2023-03-11 16:43:15 +0330

    server > fix checkValidat ErrorHandler

--- server/app/middlewares/checkValidat.middleware.js
+++ server/app/middlewares/checkValidat.middleware.js
@@ -3,11 +3,17 @@ const { StatusCodes } = require("http-status-codes");
 
 function checkValidat(req, res, next) {
     const result = validationResult(req);
+    let errors = [];
+    result.errors.forEach(error => {
+        const errorNessage = error.msg;
+        errors.push(errorNessage);
+    });
+    errors = [...new Set(errors)];
     if(!Object.keys(result?.errors).length == 0) {
         return res.status(StatusCodes.BAD_REQUEST).json({
             StatusCode: StatusCodes.BAD_REQUEST,
             data: {},
-            errors: result
+            errors: errors
         })
     }
     next();
--- server/app/middlewares/checkValidatFile.middleware.js
+++ server/app/middlewares/checkValidatFile.middleware.js
@@ -3,12 +3,18 @@ const { StatusCodes } = require("http-status-codes");
 const fs = require("fs");
 function checkValidatFile(req, res, next) {
     const result = validationResult(req);
+    let errors = [];
+    result.errors.forEach(error => {
+        const errorNessage = error.msg;
+        errors.push(errorNessage);
+    });
+    errors = [...new Set(errors)];
     if(!Object.keys(result?.errors).length == 0) {
         if (req.file) fs.unlinkSync(req.file.path); 
         return res.status(StatusCodes.BAD_REQUEST).json({
             StatusCode: StatusCodes.BAD_REQUEST,
             data: {},
-            errors: result
+            errors: errors
         })
     }
     next();
